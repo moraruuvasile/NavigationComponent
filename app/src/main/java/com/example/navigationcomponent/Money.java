ab2544e472466b86e4a7c38e479570271f31906f
Vasile <vaseoa@yahoo.com>
2020-05-02 17:15:18 +0300

    Start

---
+++ app/src/main/java/com/example/navigationcomponent/Money.java
@@ -0,0 +1,17 @@
+package com.example.navigationcomponent;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class Money  {
+
+
+}
