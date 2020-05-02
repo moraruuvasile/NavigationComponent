ff2c5fc4b078d0dd57fd053be9017ad0f92b27d1
Vasile <vaseoa@yahoo.com>
2020-05-02 20:03:14 +0300

    finish

--- app/src/main/java/com/example/navigationcomponent/MainFragment.java
+++ app/src/main/java/com/example/navigationcomponent/MainFragment.java
@@ -5,13 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class MainFragment extends Fragment {
+public class MainFragment extends Fragment implements View.OnClickListener {
+	NavController navController;
 
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -21,5 +24,29 @@ public class MainFragment extends Fragment {
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+		navController = Navigation.findNavController(view);
+
+		view.findViewById(R.id.btn_send_money).setOnClickListener(this);
+		view.findViewById(R.id.btn_view_balance).setOnClickListener(this);
+		view.findViewById(R.id.btn_view_transactions).setOnClickListener(this);
+
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+
+		switch (id) {
+			case R.id.btn_send_money:
+				navController.navigate(R.id.action_mainFragment_to_chooseRecipientFragment);
+				break;
+			case R.id.btn_view_balance:
+				navController.navigate(R.id.action_mainFragment_to_viewBalanceFragment);
+				break;
+			case R.id.btn_view_transactions:
+				navController.navigate(R.id.action_mainFragment_to_viewTransactionFragment);
+				break;
+
+		}
 	}
 }
--- app/src/main/java/com/example/navigationcomponent/SpecifyAmountFragment.java
+++ app/src/main/java/com/example/navigationcomponent/SpecifyAmountFragment.java
@@ -5,14 +5,16 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-public class SpecifyAmountFragment extends Fragment {
-
+public class SpecifyAmountFragment extends Fragment implements View.OnClickListener {
+	NavController navController;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_specify_amount, container, false);
@@ -21,5 +23,25 @@ public class SpecifyAmountFragment extends Fragment {
 	@Override
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
+		navController = Navigation.findNavController(view);
+		view.findViewById(R.id.send_btn).setOnClickListener(this);
+		view.findViewById(R.id.cancel_btn).setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		int id = v.getId();
+
+		switch (id) {
+			case R.id.send_btn:
+				navController.navigate(R.id.action_specifyAmountFragment_to_confirmationFragment);
+				break;
+			case R.id.cancel_btn:
+				getActivity().onBackPressed();
+				break;
+
+
+		}
+
 	}
 }
